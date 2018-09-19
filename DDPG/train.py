a79b74338683359540ccbd6ff202170b4c4ed9de
kev <kevin.p.okeeffee@gmail.com>
2018-09-18 22:54:57 -0400

    small adjustments

---
+++ DDPG/train.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import gym
+from agent import Agent
+from keras.utils import to_categorical
+%matplotlib inline
+
+
+#Environment
+env = gym.make('MountainCarContinuous-v0')
+env.seed(1)  # for comparison
+num_states = env.observation_space.shape[0]
+num_actions = env.action_space.shape[0]
+
+
+#Agent
+lr,gamma = 0.001, 0.99
+agent = Agent(num_states, num_actions, lr, gamma)
+agent.epsilon = 0.8
+agent.memory_size = 10**4
+epsilon_min = 0.01
+decay_factor = 0.99
+agent.tau = 0.1
+learning_start = 1000
+
+
+#Train
+EPISODES = 500
+scores = []
+step = 0
+for e in range(1,EPISODES+1):
+    state = env.reset()
+    reward_sum = 0
+    done = False
+    while not done:
+        
+        # env.render()
+        state = np.reshape(state, [1, num_states])  #reshape for keras
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        reward_sum += reward
+        agent.remember(state[0], action, reward, next_state, done)
+        state = next_state
+        
+        if len(agent.memory) > learning_start:
+            agent.train_models()
+            
+        if done:
+            break
+    
+    #Learn & print results
+    scores.append(reward_sum)
+    agent.epsilon = max(agent.epsilon*decay_factor,epsilon_min)
+    if e % 10 == 0:
+        print '(episode, score) = ' + str((e,reward_sum))
+
+plt.plot(scores)
+np.savetxt('stats/scores_inverted_pendulum.txt',scores)
